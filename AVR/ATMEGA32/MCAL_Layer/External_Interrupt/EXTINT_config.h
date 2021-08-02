984560480d0fc88c70611d1ec9a002bd4c4df217
AmrElsaied <55893916+AmrElsaied@users.noreply.github.com>
2021-08-02 22:23:16 +0200

    Add files via upload

---
+++ AVR/ATMEGA32/MCAL_Layer/External_Interrupt/EXTINT_config.h
@@ -0,0 +1,13 @@
+/*******************************************************************************************/
+/*  Author      : Amr Mohamed                                                              */
+/*  Date        : 5 Sep 2019                                                               */
+/*  Version     : 1.0 V                                                                    */
+/*******************************************************************************************/
+
+#ifndef EXTINT_CONFIG_H_
+#define EXTINT_CONFIG_H_
+
+#define INT0_MOD		MOD_FALLEDGE
+#define INT1_MOD		MOD_FALLEDGE
+#define INT2_MOD		MOD_FALLEDGE
+#endif /* EXTINT_CONFIG_H_ */
